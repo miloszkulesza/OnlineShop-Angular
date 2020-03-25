727f8bca9bf99dc75ba4084a49bd0b7f627b6bff
miloszkulesza <milosz.kulesza1@gmail.com>
2020-03-25 21:19:49 +0100

    zmiana zaznaczenia przy klikaniu po kategoriach

--- src/app/categories-list/categories-list.component.ts
+++ src/app/categories-list/categories-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Inject } from '@angular/core';
 import Category from 'src/models/Category';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'categories-list',
@@ -11,8 +12,20 @@ export class CategoriesListComponent implements OnInit {
   @Input() fetchMessageResponse: string;
   @Input() categories: Array<Category>;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document) { }
 
   ngOnInit(): void {
+  }
+
+  handleSelectedCategoryChanged(event): void {
+    let categoryId = event.target.id;
+    let previousSelection = document.getElementsByClassName('selectedCategory')[0];
+    previousSelection.classList.remove('selectedCategory');
+    previousSelection.classList.remove('bg-primary');
+    previousSelection.classList.remove('text-white');
+    let selectedCategory = document.getElementById(categoryId);
+    selectedCategory.classList.add('selectedCategory');
+    selectedCategory.classList.add('bg-primary');
+    selectedCategory.classList.add('text-white');
   }
 }
